d88ad736255a697aa298996af5ec6368ed29d1d5
Your Name <adammhancock@gmail.com>
2018-02-01 10:07:13 +0000

    Fixed spacing.

--- dinopassgenerator.js
+++ dinopassgenerator.js
@@ -10,8 +10,8 @@ switch (strong){
     case false:
         url = 'http://www.dinopass.com/password/simple';
     break;
-    case true:
-    url = 'http://www.dinopass.com/password/strong';
+        case true:
+        url = 'http://www.dinopass.com/password/strong';
     break; 
 }
 
@@ -20,13 +20,15 @@ url: url};
 
 var request = require("request");
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
- for(var i = 0; i < passwords;i++){
+            function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
 
-  console.log(capitalizeFirstLetter(body));
+for(var i = 0; i < passwords;i++){
+
+                    request(options, function (error, response, body) {
+                    if (error) throw new Error(error);
+
+                    console.log(capitalizeFirstLetter(body));
 })};

d3ed61438c1c0968cff152d138773f22734d6da9
Adam Hancock <adammhancock@gmail.com>
2018-02-05 10:54:40 +0000

    Converted to class

--- dinopassgenerator.js
+++ dinopassgenerator.js
@@ -1,41 +1,54 @@
+
 // Adam Hancock
 // Run with node dinopassgenerator.js <number of passwords>
 // 
+var type = "simple"; // simple or strong. 
 
-var strong = false
-
-var passwords = process.argv['2']
 ////////////////////////////////////////////////////////////////
+var request = require('request')
+class dinopass {
+   
+    constructor(passwords, type){
+        this.passwords = passwords
 
 
+        switch (type){
+            case "simple":
+                this.url = 'http://www.dinopass.com/password/simple';
+            break;
+                case "strong":
+                this.url = 'http://www.dinopass.com/password/strong';
+            break; 
+        }
 
-var request = require("request");
-switch (strong){
-    case false:
-        url = 'http://www.dinopass.com/password/simple';
-    break;
-        case true:
-        url = 'http://www.dinopass.com/password/strong';
-    break; 
+
+    }
+
+    call(passwords, url){
+        var passwords = this.passwords;
+        var url = this.url;
+        if (typeof passwords == 'undefined') {
+            var passwords = '45';
+           }
+         
+         var options = { method: 'GET',
+         url: url};
+         
+         
+                     function capitalizeFirstLetter(string) {
+                         return string.charAt(0).toUpperCase() + string.slice(1);
+                     }
+         
+         
+         for(var i = 0; i < passwords;i++){
+         
+                             request(options, function (error, response, body) {
+                             if (error) throw new Error(error);
+         
+                             console.log(capitalizeFirstLetter(body));
+         })}
+    }
 }
-// If no passwords set by command line argument, return 45 passwords. 
-if (typeof passwords == 'undefined') {
-   var passwords = '45';
-  }
 
-var options = { method: 'GET',
-url: url};
-
-
-            function capitalizeFirstLetter(string) {
-                return string.charAt(0).toUpperCase() + string.slice(1);
-            }
-
-
-for(var i = 0; i < passwords;i++){
-
-                    request(options, function (error, response, body) {
-                    if (error) throw new Error(error);
-
-                    console.log(capitalizeFirstLetter(body));
-})};
+let dinopassgenerator = new dinopass(process.argv['2'], type);
+dinopassgenerator.call();
